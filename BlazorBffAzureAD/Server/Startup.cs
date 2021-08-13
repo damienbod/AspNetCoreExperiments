5646335f896aa8c30bb1d94bc32c939d232866d4
damienbod <damien_bod@hotmail.com>
2021-08-13 11:21:14 +0200

    code clean up

--- BlazorBffAzureAD/Server/Startup.cs
+++ BlazorBffAzureAD/Server/Startup.cs
@@ -1,19 +1,7 @@
 using BlazorHosted.Server.Services;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Authorization;
-using Microsoft.AspNetCore.ResponseCompression;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
-using System.Linq;
 
 namespace BlazorHosted.Server
 {
@@ -26,8 +14,6 @@ namespace BlazorHosted.Server
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<GraphApiClientService>();
@@ -63,7 +49,6 @@ namespace BlazorHosted.Server
             }).AddMicrosoftIdentityUI();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
