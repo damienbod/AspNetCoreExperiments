0d248189ec10fd8b5ed4211db4579ab5e2dc2fed
damienbod <damien_bod@hotmail.com>
2021-09-15 08:13:01 +0200

    Updating packages

--- BlazorBffAzureAD/Server/Startup.cs
+++ BlazorBffAzureAD/Server/Startup.cs
@@ -1,5 +1,10 @@
 using BlazorHosted.Server.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
 
