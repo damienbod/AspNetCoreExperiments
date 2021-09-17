40dbdb5114bdfc749b391fce4a4626a1429a1569
damienbod <damien_bod@hotmail.com>
2021-09-17 08:08:39 +0200

    init project

---
+++ BlazorBffAzureADWithApi/Server/SecurityHeadersDefinitions.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace BlazorHosted.Server
+{
+    public static class SecurityHeadersDefinitions
+    {
+        public static HeaderPolicyCollection GetHeaderPolicyCollection(bool isDev, string idpHost)
+        {
+            var policy = new HeaderPolicyCollection()
+                .AddFrameOptionsDeny()
+                .AddXssProtectionBlock()
+                .AddContentTypeOptionsNoSniff()
+                .AddReferrerPolicyStrictOriginWhenCrossOrigin()
+                .RemoveServerHeader()
+                .AddCrossOriginOpenerPolicy(builder =>
+                {
+                    builder.SameOrigin();
+                })
+                .AddCrossOriginEmbedderPolicy(builder =>
+                {
+                    builder.RequireCorp();
+                })
+                .AddCrossOriginResourcePolicy(builder =>
+                {
+                    builder.SameOrigin();
+                })
+                .AddContentSecurityPolicy(builder =>
+                {
+                    builder.AddObjectSrc().None();
+                    builder.AddBlockAllMixedContent();
+                    builder.AddImgSrc().Self().From("data:");
+                    builder.AddFormAction().Self().From(idpHost);
+                    builder.AddFontSrc().Self();
+                    builder.AddStyleSrc().Self().UnsafeInline();
+                    builder.AddBaseUri().Self();
+                    builder.AddFrameAncestors().None();
+
+                    // due to Blazor
+                    builder.AddScriptSrc().Self().UnsafeInline().UnsafeEval();
+
+                })
+                .RemoveServerHeader()
+                .AddPermissionsPolicy(builder =>
+                {
+                    builder.AddAccelerometer().None();
+                    builder.AddAutoplay().None();
+                    builder.AddCamera().None();
+                    builder.AddEncryptedMedia().None();
+                    builder.AddFullscreen().All();
+                    builder.AddGeolocation().None();
+                    builder.AddGyroscope().None();
+                    builder.AddMagnetometer().None();
+                    builder.AddMicrophone().None();
+                    builder.AddMidi().None();
+                    builder.AddPayment().None();
+                    builder.AddPictureInPicture().None();
+                    builder.AddSyncXHR().None();
+                    builder.AddUsb().None();
+                });
+
+            if (!isDev)
+            {
+                // maxage = one year in seconds
+                policy.AddStrictTransportSecurityMaxAgeIncludeSubDomains(maxAgeInSeconds: 60 * 60 * 24 * 365);
+            }
+
+            return policy;
+        }
+    }
+}
