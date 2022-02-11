9229a974167945347c9f98000029fe436a690a92
damienbod <damien_bod@hotmail.com>
2022-02-11 08:06:12 +0100

    Update namespaces

--- BlazorBffAzureAD/Server/Controllers/DirectApiController.cs
+++ BlazorBffAzureAD/Server/Controllers/DirectApiController.cs
@@ -4,19 +4,18 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Web;
 
-namespace BlazorHosted.Server.Controllers
+namespace BlazorHosted.Server.Controllers;
+
+[ValidateAntiForgeryToken]
+[Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+[AuthorizeForScopes(Scopes = new string[] { "api://b2a09168-54e2-4bc4-af92-a710a64ef1fa/access_as_user" })]
+[ApiController]
+[Route("api/[controller]")]
+public class DirectApiController : ControllerBase
 {
-    [ValidateAntiForgeryToken]
-    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
-    [AuthorizeForScopes(Scopes = new string[] { "api://b2a09168-54e2-4bc4-af92-a710a64ef1fa/access_as_user" })]
-    [ApiController]
-    [Route("api/[controller]")]
-    public class DirectApiController : ControllerBase
+    [HttpGet]
+    public IEnumerable<string> Get()
     {
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new List<string> { "some data", "more data", "loads of data" };
-        }
+        return new List<string> { "some data", "more data", "loads of data" };
     }
 }
