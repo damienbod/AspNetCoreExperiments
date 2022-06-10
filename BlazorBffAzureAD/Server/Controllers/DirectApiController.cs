d210f74aecea8df331beac10b549ec088b4f1736
damienbod <damien_bod@hotmail.com>
2022-06-10 21:09:06 +0200

    Update BFF project

--- BlazorBffAzureAD/Server/Controllers/DirectApiController.cs
+++ BlazorBffAzureAD/Server/Controllers/DirectApiController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
 
 namespace BlazorHosted.Server.Controllers;
 
